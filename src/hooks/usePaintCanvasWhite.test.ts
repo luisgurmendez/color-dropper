6f6a1dbaf7cbe9fdf458178ee107c2e877a8eb85
Luis Gurmendez <luisgurmendez@gmail.com>
2023-02-06 20:46:58 +0100

    remove unused test

--- src/hooks/usePaintCanvasWhite.test.ts
+++
@@ -1,6 +0,0 @@
-import { renderHook } from '@testing-library/react-hooks'
-// import usePaintCanvasWhite from './usePaintCanvasWhite'
-
-test('should use counter', () => {
-    expect(true).toBeTruthy()
-})
